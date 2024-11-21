0ab3991ee025f4efd4c12d6d2eab85325ef958d6
Porrasm86 <153502699+Porrasm86@users.noreply.github.com>
2024-11-21 09:59:04 -0700

    Delete js directory

--- js/example-1.js
+++
@@ -1,7 +0,0 @@
-(function() {
-var container = document.querySelector('#example1');
-var root1 = container.createShadowRoot();
-var root2 = container.createShadowRoot();
-root1.innerHTML = '<div>Первый корневой элемент</div>';
-root2.innerHTML = '<div>Второй корневой элемент</div>';
-})();
--- js/example-2.js
+++
@@ -1,7 +0,0 @@
-(function() {
-var container = document.querySelector('#example2');
-var root1 = container.createShadowRoot();
-var root2 = container.createShadowRoot();
-root1.innerHTML = '<div>Первый корневой элемент</div><content></content>';
-root2.innerHTML = '<div>Второй корневой элемент</div><shadow></shadow>';
-})();
